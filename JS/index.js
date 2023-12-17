98fd4b2493e3b64ff5f2e7bbcd5fbfbdf65c095e
despejo11 <stepanciocoboc@gmail.com>
2023-12-17 17:05:39 +0200

    fix bugs, added section

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,5 @@
 function disableScroll() {
 	var scrollPosition = window.scrollY || window.pageYOffset
-
 	document.body.style.overflowY = 'hidden'
 	document.body.style.position = 'fixed'
 	document.body.style.top = `-${scrollPosition}px`
@@ -8,32 +7,48 @@ function disableScroll() {
 
 function enableScroll() {
 	var scrollPosition = parseInt(document.body.style.top, 10)
-
 	document.body.style.overflowY = ''
 	document.body.style.position = ''
 	document.body.style.top = ''
-
 	window.scrollTo(0, scrollPosition)
 }
 
-var burgerMenu = document.querySelector('.burger-menu')
 var openButton = document.querySelector('.menu-btn')
 var menubox = document.querySelector('.menubox')
+var menuToggle = document.getElementById('menu-toggle')
 
 openButton.addEventListener('click', function () {
 	disableScroll()
 	menubox.style.left = '0'
 })
 
+function closeMenu() {
+	enableScroll()
+	menuToggle.checked = false
+	menubox.style.left = '-100%'
+}
+
 document.addEventListener('click', function (event) {
-	if (!burgerMenu.contains(event.target) && !menubox.contains(event.target)) {
-		enableScroll()
-		document.getElementById('menu-toggle').checked = false
-		menubox.style.left = '-100%'
+	if (
+		!(
+			event.target.matches('.burger-menu') ||
+			event.target.closest('.burger-menu')
+		)
+	) {
+		if (window.innerWidth <= 1025) {
+			closeMenu()
+		}
 	}
 })
 
-document.getElementById('menu-toggle').addEventListener('change', function () {
+window.addEventListener('resize', function () {
+	if (window.innerWidth > 1025) {
+		enableScroll()
+		closeMenu()
+	}
+})
+
+menuToggle.addEventListener('change', function () {
 	if (this.checked) {
 		disableScroll()
 	} else {
