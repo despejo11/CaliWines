62b51737e4f416b05cd127b45780bc2cc92c5f52
despejo11 <stepanciocoboc@gmail.com>
2023-12-17 20:09:06 +0200

    fix bugs, added sections

--- JS/index.js
+++ JS/index.js
@@ -13,9 +13,9 @@ function enableScroll() {
 	window.scrollTo(0, scrollPosition)
 }
 
-var openButton = document.querySelector('.menu-btn')
-var menubox = document.querySelector('.menubox')
-var menuToggle = document.getElementById('menu-toggle')
+let openButton = document.querySelector('.menu-btn')
+let menubox = document.querySelector('.menubox')
+let menuToggle = document.getElementById('menu-toggle')
 
 openButton.addEventListener('click', function () {
 	disableScroll()
@@ -29,24 +29,62 @@ function closeMenu() {
 }
 
 document.addEventListener('click', function (event) {
+	handleMenuClick(event)
+})
+
+document.addEventListener('touchstart', function (event) {
+	handleMenuClick(event)
+})
+
+function handleMenuClick(event) {
 	if (
 		!(
 			event.target.matches('.burger-menu') ||
-			event.target.closest('.burger-menu')
+			event.target.closest('.burger-menu') ||
+			event.target.matches('.menubox') ||
+			event.target.closest('.menubox')
 		)
 	) {
-		if (window.innerWidth <= 1025) {
+		if (window.innerWidth <= 1025 && menuToggle.checked) {
 			closeMenu()
+			enableScroll()
+			menuToggle.checked = false
+			event.preventDefault()
 		}
+	}
+}
+
+menuToggle.addEventListener('change', function () {
+	if (this.checked) {
+		disableScroll()
+	} else {
+		enableScroll()
 	}
 })
 
+var isMenuOpen = false
+
 window.addEventListener('resize', function () {
-	if (window.innerWidth > 1025) {
+	if (window.innerWidth > 1025 && isMenuOpen) {
 		enableScroll()
 		closeMenu()
+		var currentScrollPos = window.scrollY || window.pageYOffset
+		window.scrollTo(0, currentScrollPos)
 	}
 })
+
+openButton.addEventListener('click', function () {
+	disableScroll()
+	isMenuOpen = true
+	menubox.style.left = '0'
+})
+
+function closeMenu() {
+	enableScroll()
+	isMenuOpen = false
+	menuToggle.checked = false
+	menubox.style.left = '-100%'
+}
 
 menuToggle.addEventListener('change', function () {
 	if (this.checked) {
